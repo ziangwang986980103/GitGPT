6b42862b6133f23a9cefd329e575c2c5190a0d60
ziangwang986980103 <ziangw@umich.educd>
2023-10-30 22:32:25 -0400

    commit before deploying

--- server/server.js
+++ server/server.js
@@ -55,10 +55,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use(express.static('public'));
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 /**
@@ -588,9 +588,9 @@ app.post('/api/answer-question', async (req, res) => {
     return res.json(answer);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
