f04154ce9ec9e1be923110375e1c7b0a4f77e192
ziangwang986980103 <ziangw@umich.educd>
2023-11-05 23:42:35 -0500

    commit before deploy

--- server/Prompts/ignore.js
+++ server/Prompts/ignore.js
@@ -1,4 +1,4 @@
 const file_to_be_ignored = ["node_modules", "package-lock.json", "yarn.lock", "poetry.lock",".egg-info","LICENSE",".gitignore","CONTRIBUTING.md","CONTRIBUTORS"
-,"go.sum","CONDUCT.md","AUTHORS",".xlsx"];
+,"go.sum","CONDUCT.md","AUTHORS",".xlsx",".csv","lecture"];
 
 export default file_to_be_ignored;
