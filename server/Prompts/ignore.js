e64a410389d194968159eb7035fef84e38866ab2
ziangwang986980103 <ziangw@umich.educd>
2023-11-05 23:35:54 -0500

    add .xlsx to ignore.js

--- server/Prompts/ignore.js
+++ server/Prompts/ignore.js
@@ -1,4 +1,4 @@
 const file_to_be_ignored = ["node_modules", "package-lock.json", "yarn.lock", "poetry.lock",".egg-info","LICENSE",".gitignore","CONTRIBUTING.md","CONTRIBUTORS"
-,"go.sum","CONDUCT.md","AUTHORS"];
+,"go.sum","CONDUCT.md","AUTHORS",".xlsx"];
 
 export default file_to_be_ignored;
